18fe6ac5dbed566f8326ffdc6a64ef31438a871b
Adnan Joraid <adnanjoraidca@gmail.com>
2020-07-24 20:58:22 -0400

    Initial Commit

---
+++ 11.ChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  11.ChatApp
+//
+//  Created by Adnan Joraid on 2020-07-24.
+//  Copyright © 2020 Adnan Joraid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
